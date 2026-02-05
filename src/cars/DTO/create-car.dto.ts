947e939f6ba2447699342c91db0301fbfcf95010
Omar Gerardo Servin Rivera <oservin@izzi.mx>
2026-02-04 18:50:02 -0600

    Seccion4-ValidationPipe

--- src/cars/DTO/create-car.dto.ts
+++ src/cars/DTO/create-car.dto.ts
@@ -1,4 +1,9 @@
+import { IsString } from 'class-validator';
+
 export class CreateCarDto {
+  @IsString({ message: `modelo debe de ser sting` })
   readonly brand: string;
+
+  @IsString()
   readonly model: string;
 }
